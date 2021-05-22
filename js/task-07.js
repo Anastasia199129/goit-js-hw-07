791b79b8eded986ed493c95d4b0d0d8f42ef2284
Anastasia <nastasia199126@gmail.com>
2021-05-22 20:21:47 +0300

    my commit

--- js/task-07.js
+++ js/task-07.js
@@ -1,4 +1,10 @@
 const inputEl = document.querySelector('#font-size-control')
 console.log(inputEl)
+const spanEl = document.querySelector('#text')
 
 inputEl.addEventListener('input', onInputBlur)
+
+function onInputBlur(event){
+spanEl.style.fontSize = '30px'
+console.log(spanEl)
+}
